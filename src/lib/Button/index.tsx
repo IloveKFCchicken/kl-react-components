8605c47c144961d1c641d34f2db71ee193d6c83a
IloveKFCchicken <619487583@qq.com>
2020-04-01 11:56:38 +0800

    feat: button update

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -5,14 +5,26 @@ import './index.less'
 
 interface ButtonProps {
   children: JSX.Element[] | JSX.Element,
-  className: string
+  className: string,
+  type: 'default' | 'primary' | 'error' | 'warn' | 'success',
+  size: 's' | 'm' | 'l',
+  ripple: boolean,
+  onClick: Function
 }
 
-export default (props: ButtonProps) => {
-  const { children, className } = props
+Button.defaultProps = {
+  type: 'default',
+  size: 'm',
+  ripple: true
+} as Partial<ButtonProps>;
+
+export default function Button(props: ButtonProps) {
+  const { children, className, type, size, ripple } = props
   return (
-    <button className={cn("kl-button app-component", className && className)} {...props}>
-      {children}
+    <button className={cn("kl-component kl-button", className && className, type && `button--${type}`, size && `button-size--${size}`)}>
+      <span className='button--children'>{children}</span>
+      {ripple && <span className='button--ripple'></span>}
     </button>
   )
 }
+
